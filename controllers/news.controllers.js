4aad602191447997ee27a8179fc59945bb55d017
sofiabezertzi <smpezert@me.com>
2022-07-04 16:47:21 +0100

    adds changes

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -3,7 +3,6 @@ const { selectTopics } = require("../models/news.models");
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((topic) => {
-      // console.log(res);
       res.status(200).send({ topics: topic });
     })
     .catch((error) => {
