53a1db05986870d416476db7b50291710bd75054
sofiabezertzi <smpezert@me.com>
2024-02-14 20:44:55 +0000

    remove the heroku from the script

--- db/connection.js
+++ db/connection.js
@@ -9,14 +9,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 module.exports = new Pool(config);
